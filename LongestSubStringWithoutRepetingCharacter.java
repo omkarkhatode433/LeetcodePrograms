01afaa8daf3edc980e093c5a990c3aa30cb61ee2
omkar khatode <omkarkhatode357@gmail.com>
2023-10-20 21:30:31 +0530

    LongestSubStringWithoutRepetingCharacter program added

---
+++ LongestSubStringWithoutRepetingCharacter.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+
+public class LongestSubStringWithoutRepetingCharacter{
+    public static void main(String[] args) {
+        
+    }
+    
+}
+class Solution {
+   
+    int m=0;
+    public int lengthOfLongestSubstring(String s) {
+        ArrayList<Integer> arr=new ArrayList<>();
+        
+        if(s.length()==1){
+            return 1;
+        }
+        for(int i=0;i<s.length();i++){
+            StringBuilder sb=new StringBuilder();
+            for(int j=i;j<s.length();j++){
+                if(sb.indexOf(s.charAt(j)+"")==-1)
+                sb.append(s.charAt(j));
+                else
+                break;
+
+            }
+           
+           if(sb.length()>m){
+               m=sb.length();
+           }
+            
+        }
+        return m;
+    }
+}
