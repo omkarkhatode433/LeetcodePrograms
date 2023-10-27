f6576e69d9b7f8bc60e83dd2e3413b667bb58f36
omkar khatode <omkarkhatode357@gmail.com>
2023-10-28 01:30:11 +0530

    fibonacii

---
+++ Fibbonacci.java
@@ -0,0 +1,13 @@
+public class Fibbonacci {
+    
+}
+class Solution {
+    public int fib(int n) {
+        if(n==1||n==0){
+            return n;
+        }else{
+            return fib(n-1)+fib(n-2);
+        }
+        
+    }
+}
