38b9b260903ca8b2992ab0d855ba98c01ee10668
omkar khatode <omkarkhatode357@gmail.com>
2023-10-27 23:55:42 +0530

    Move zero

---
+++ MoveZero.java
@@ -0,0 +1,20 @@
+public class MoveZero {
+    
+}
+
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int idx=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]!=0){
+                nums[idx]=nums[i];
+                idx++;
+            }
+
+        }
+        for(int i=idx;i<nums.length;i++){
+            nums[i]=0;
+        }
+        
+    }
+}
