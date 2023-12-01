a43ed92cc1a1d16f408ec5fd5a4e48ce74308277
omkar khatode <omkarkhatode357@gmail.com>
2023-12-01 20:43:00 +0530

    Number of Pairs of Strings With Concatenation Equal to Target

---
+++ Pairs.java
@@ -0,0 +1,15 @@
+public class Pairs {
+    
+}
+class Solution {
+    public int numOfPairs(String[] nums, String target) {
+        int c=0;
+        for(int i=0;i<nums.length;i++){
+            for(int j=0;j<nums.length;j++){
+                if(i!=j&&(nums[i]+nums[j]).equals(target))c++;
+            }
+        }
+        return c;
+        
+    }
+}
